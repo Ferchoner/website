f2d69b60ecef76679a5059f2525bcea94da687bd
fhernandez <fhernandez@ia.com.mx>
2016-08-23 13:50:55 -0500

    Creando primer prueba

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,15 +12,15 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/home", name="homepage")
-     * @Template("AppBundle:Default:number.html.twig")
+     * @Template("AppBundle:Default:home.html.twig")
      *
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return []; /*$this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        ]);*/
     }
 
 }
